ff3aa345d843aaed40f24aaa29d385c64a938a5c
Rodywody <168793009+Rodywody@users.noreply.github.com>
2024-06-07 16:17:12 +0900

    Update Napalm.py

--- Network/Napalm.py
+++ Network/Napalm.py
@@ -9,3 +9,16 @@ config = device.get_config()
 device.close()
 
 print(config)
+
+
+
+
+from napalm import get_network_driver
+driver = get_network_driver('ios')
+device = driver(nostname='10.1.1.11', username='ccnp', password='cisco')
+device.open()
+interface = device.get_interfaces()
+print (interface)
+device.close()
+
+
